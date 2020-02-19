10f9b5bb93b9e7c055d3686eecc8fe4d3d079ec2
Michael Huwiler <michael.huwiler@schindler.com>
2020-02-19 08:14:22 +0100

    Setup Files

--- setup.py
+++ setup.py
@@ -8,11 +8,13 @@ setup(
   author = 'Michael Huwiler',                   # Type in your name
   author_email = 'michael.huwiler@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/mikehummell/bidataloader',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/mikehummell/bidataloader/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['BI', 'SQL', 'Alchemy'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'sqlalchemy',
+          'urllib',
+          'pandas',
+          'pyodbc',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
